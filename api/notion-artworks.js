cb85e2e3f9bb591478c2ac22e1e09c58306c3c03
Glanfaloth <yeltao@student.ethz.ch>
2025-07-17 23:26:36 +0200

    notion database

---
+++ api/notion-artworks.js
@@ -0,0 +1,31 @@
+import { Client } from "@notionhq/client";
+
+export default async function handler(req, res) {
+  const notion = new Client({ auth: process.env.NOTION_API_KEY });
+  const databaseId = process.env.NOTION_DATABASE_ID;
+
+  try {
+    const response = await notion.databases.query({ database_id: databaseId });
+
+    // Map Notion results to artwork structure
+    const artworks = response.results.map(page => {
+      const props = page.properties;
+      return {
+        id: page.id,
+        title: props.Title.rich_text?.[0].plain_text || "",
+        description: props.Description.rich_text?.[0].plain_text || "",
+        images: props.Images.url || "",
+        date: props.Date.date?.start || "",
+        location: props.Location.rich_text?.[0].plain_text || "",
+        position: [
+          parseFloat(props.Latitude.number),
+          parseFloat(props.Longitude.number)
+        ]
+      };
+    });
+
+    res.status(200).json(artworks);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,8 @@
+import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import './App.css';
-
-const artworks = [
-  {
-    id: 1,
-    title: 'Sketch in Paris',
-    description: 'A quick sketch of the Eiffel Tower.',
-    image: 'https://en.wikipedia.org/wiki/Eiffel_Tower#/media/File:Tour_Eiffel_Wikimedia_Commons_(cropped).jpg',
-    position: [48.8584, 2.2945],
-  },
-  {
-    id: 2,
-    title: 'New York Street',
-    description: 'Urban sketching in Manhattan.',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/4/4d/Manhattan_Skyline_2006.jpg',
-    position: [40.7128, -74.006],
-  },
-  {
-    id: 3,
-    title: 'Tokyo Temple',
-    description: 'A peaceful temple in Tokyo.',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/1/1e/Sensoji_Temple%2C_Tokyo%2C_Japan.jpg',
-    position: [35.7148, 139.7967],
-  },
-];
 
 // Fix default icon issue in Leaflet with React
 import icon from 'leaflet/dist/images/marker-icon.png';
@@ -37,6 +14,14 @@ let DefaultIcon = L.icon({
 L.Marker.prototype.options.icon = DefaultIcon;
 
 function App() {
+  const [artworks, setArtworks] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/notion-artworks")
+      .then(res => res.json())
+      .then(data => setArtworks(data));
+  }, []);
+
   return (
     <div style={{ height: '100vh', width: '100vw' }}>
       <MapContainer center={[20, 0]} zoom={2} style={{ height: '100%', width: '100%' }}>
@@ -48,9 +33,11 @@ function App() {
           <Marker key={art.id} position={art.position}>
             <Popup>
               <div style={{ maxWidth: 200 }}>
-                <img src={art.image} alt={art.title} style={{ width: '100%', borderRadius: 4 }} />
+                <img src={art.images} alt={art.title} style={{ width: '100%', borderRadius: 4 }} />
                 <h3>{art.title}</h3>
                 <p>{art.description}</p>
+                <p><b>Date:</b> {art.date}</p>
+                <p><b>Location:</b> {art.location}</p>
               </div>
             </Popup>
           </Marker>
